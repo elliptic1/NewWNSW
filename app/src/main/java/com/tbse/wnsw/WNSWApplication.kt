6cff182f13d751e7e9d2f241890857405ebd3073
Todd B. Smith <elliptic1@gmail.com>
2021-05-19 23:01:30 -0400

    Put Hilt on Application

--- app/src/main/java/com/tbse/wnsw/WNSWApplication.kt
+++ app/src/main/java/com/tbse/wnsw/WNSWApplication.kt
@@ -1,10 +1,12 @@
 package com.tbse.wnsw
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * Created by toddsmith on 5/19/21.
  * Copyright TBSE 2017
  */
+@HiltAndroidApp
 class WNSWApplication : Application() {
 }
