468329a2eacab9103ae32a4c4504275bd8d8c4fe
Todd B. Smith <elliptic1@gmail.com>
2021-12-13 03:15:27 -0500

    Add touch events to turn Rows green when touching

--- app/src/main/java/com/tbse/wnsw/ui/aplist/item/AccessPointListItem.kt
+++ app/src/main/java/com/tbse/wnsw/ui/aplist/item/AccessPointListItem.kt
@@ -1,6 +1,7 @@
 package com.tbse.wnsw.ui.aplist.item
 
 import android.net.wifi.WifiNetworkSuggestion
+import android.view.MotionEvent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,7 +9,11 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -21,6 +26,7 @@ import com.tbse.wnsw.ui.aplist.preview.AccessPointPreviewProvider
  * Created by toddsmith on 5/15/21.
  * Copyright TBSE 2022
  */
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun AccessPointListItem(
@@ -30,11 +36,28 @@ fun AccessPointListItem(
     addNetworkSuggestions: (List<WifiNetworkSuggestion>) -> Unit = {},
     removeNetworkSuggestions: (List<WifiNetworkSuggestion>) -> Unit = {},
 ) {
+    var isClicked = remember { mutableStateOf(false) }
+
+    val bg = colorResource(
+        if (isClicked.value.not()) {
+            R.color.grey
+        } else {
+            R.color.green
+        }
+    )
     Row(
         Modifier
-            .background(colorResource(R.color.grey))
+            .background(bg)
             .fillMaxWidth()
             .height(90.dp)
+            .pointerInteropFilter {
+                isClicked.value = when (it.action) {
+                    MotionEvent.ACTION_DOWN -> true
+                    MotionEvent.ACTION_MOVE -> true
+                    else -> false
+                }
+                true
+            }
     ) {
         Column(
             Modifier
