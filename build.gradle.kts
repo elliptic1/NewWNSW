e5c87f54707ee213f2d6edb8b70b213fce68c5ea
Todd B. Smith <elliptic1@gmail.com>
2025-06-19 13:31:45 -0400

    Remove bundled Gradle wrapper JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.0.0-beta01")
+    val compose_version by extra("1.5.4")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
